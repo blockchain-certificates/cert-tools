5e505bb6e133a61744aeec25a29cdc6771e91ecd
Kim Hamilton Duffy <kimdhamilton@gmail.com>
2016-09-06 15:39:55 -0700

    test framework

---
+++ setup.py
@@ -0,0 +1,27 @@
+import os
+import uuid
+
+from pip.req import parse_requirements
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+install_reqs = parse_requirements(os.path.join(here, 'requirements.txt'), session=uuid.uuid1())
+reqs = [str(ir.req) for ir in install_reqs]
+
+with open(os.path.join(here, 'README.md')) as fp:
+    long_description = fp.read()
+
+setup(
+    name='cert-tools',
+    version='1.2.0',
+    description='creates blockchain certificates',
+    author='MIT Media Lab Blockchain Certificates',
+    tests_require=['tox'],
+    url='https://github.com/blockchain-certificates/cert-tools',
+    license='MIT',
+    author_email='certs@media.mit.edu',
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=reqs
+)
---
+++ tests/test_cert_tools.py
@@ -0,0 +1,10 @@
+import unittest
+
+
+class TestCertTools(unittest.TestCase):
+    def test_fail(self):
+        self.fail('needs tests!')
+
+
+if __name__ == '__main__':
+    unittest.main()
