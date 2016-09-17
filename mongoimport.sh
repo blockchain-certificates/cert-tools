eda2ec3a0e84a4bb430eb090cec3ddba2a4714f1
Kim Hamilton Duffy <kimdhamilton@gmail.com>
2016-09-16 18:53:07 -0700

    move files to cert-transport

--- mongoimport.sh
+++
@@ -1,4 +0,0 @@
-
-
-mongoimport --db admin --collection recipients --type json --file recipients.json --jsonArray
-mongoimport --db admin --collection certificates --type json --file certificates.json --jsonArray
