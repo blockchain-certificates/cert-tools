ced155301e97fb80d11f53bbd5779cb4e5293679
kimdhamilton <kimdhamilton@gmail.com>
2018-04-29 12:54:25 -0700

    update dependencies, remove parse_requirements dependency, fix build warnings in unit tests

--- setup.py
+++ setup.py
@@ -1,15 +1,14 @@
 import os
-import uuid
 
-from pip.req import parse_requirements
 from setuptools import setup, find_packages
 
 from cert_tools import __version__
 
 here = os.path.abspath(os.path.dirname(__file__))
 
-install_reqs = parse_requirements(os.path.join(here, 'requirements.txt'), session=uuid.uuid1())
-reqs = [str(ir.req) for ir in install_reqs]
+with open('requirements.txt') as f:
+    install_reqs = f.readlines()
+    reqs = [str(ir) for ir in install_reqs]
 
 with open(os.path.join(here, 'README.md')) as fp:
     long_description = fp.read()
