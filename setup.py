8c89e64340b5eb4ca7acc9e1ff4b4d0d77e352e8
AnthonyRonning <anthonyronning@gmail.com>
2020-03-27 17:01:24 -0500

    V3 Alpha Updates

--- setup.py
+++ setup.py
@@ -29,7 +29,9 @@ setup(
     entry_points={
         'console_scripts': [
             'create-certificate-template = cert_tools.create_v2_certificate_template:main',
+            'create-certificate-template_v3 = cert_tools.create_v3_alpha_certificate_template:main',
             'instantiate-certificate-batch = cert_tools.instantiate_v2_certificate_batch:main',
+            'instantiate-certificate-batch_v3 = cert_tools.instantiate_v3_alpha_certificate_batch:main',
             'create-issuer = cert_tools.create_v2_issuer:main'
         ]}
 )
